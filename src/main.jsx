83315016a7500b7e91cc111af2b5bb49f4db6148
Manasa K Rao <raomanasa133@gmail.com>
2024-05-20 21:08:17 +0530

    Initial Commit

---
+++ src/main.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+//...
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "https://9b761dc75da888dcd6c32c2729ff660d@o4507107871162368.ingest.us.sentry.io/4507107873783808",
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.metrics.metricsAggregatorIntegration(),
+    Sentry.reactRouterV6BrowserTracingIntegration({
+      useEffect:React.useEffect,
+    }),
+    Sentry.replayIntegration({
+      maskAllText:false,
+      blockAllMedia:false,
+    }),
+  ],
+  
+  tracesSampleRate: 1.0, 
+ 
+  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+ 
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+});
+
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
